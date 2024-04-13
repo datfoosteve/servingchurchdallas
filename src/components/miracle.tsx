b3f4e50474f64191f901f46aef6285c2d1ef29ff
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2024-04-13 14:27:58 -0500

    Update image import path in miracle.tsx component

--- src/components/miracle.tsx
+++ src/components/miracle.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import Image from "next/image";
-import yourImagePath from "/src/images/miracle.png";
+import yourImagePath from "/src/images/aiminlife.svg";
 import { Separator } from "@/components/ui/separator"
 
 export function MiracleSection () {
@@ -18,12 +18,9 @@ export function MiracleSection () {
         />
         </div>
         <div className="md:w-1/2">
-          <h2 className="text-4xl font-bold mb-4">I NEED A MIRACLE</h2>
+          <h2 className="text-4xl font-bold mb-4">WHAT IS YOUR AIM IN LIFE?</h2>
           <p className="text-lg mb-8">
-            Do you need a miracle? We could all use one in some area of our 
-            lives. <span className="italic">This series tells the countless stories of Jesus healing, restoring 
-            and providing—and how Hes still doing it today!</span> Come see what His
-            miracles look like for you.
+            We will be discussing the bigger questions of life and considering what God needs of us.
           </p>
 
           <Separator className="border-gray-200 mb-8" />
