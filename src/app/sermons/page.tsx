7b839157485528c9c367ac702cde010bc0e8b290
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2024-04-22 19:45:46 -0500

    Update SermonsPage component with new layout and styles

--- src/app/sermons/page.tsx
+++ src/app/sermons/page.tsx
@@ -5,16 +5,15 @@ import { Card, CardContent, CardTitle } from '@/components/ui/card';
 
 const SermonsPage = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="flex container mx-auto px-2 py-8">
       <Card>
         <CardContent>
-          <CardTitle className="text-4xl font-bold mb-4">Sermons</CardTitle>
-          <div className="iframe-container">
+          <CardTitle className="text-4xl font-bold mb-4 content-center text-center">Sermons</CardTitle>
+          <div className="flex container w-screen">
             <iframe
               src="https://embed.acast.com/63d6855a93dfbe00116a43c6?theme=light&font-family=Poppins&font-src=https%3A%2F%2Ffonts.googleapis.com%2Fcss%3Ffamily%3DPoppins&feed=true"
               allow="autoplay; encrypted-media"
-              title="Sermons Podcast"
-              className="iframe"
+              className="flex container w-3/4 h-screen box-content border-0 overflow-hidden"
             ></iframe>
           </div>
         </CardContent>
