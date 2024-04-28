5414bd4e8be5955b2a36660d83986e4b04581ef6
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2024-04-27 20:26:32 -0500

    Refactor donate layout component and update PayPalScriptProvider options

--- src/app/donate/layout.tsx
+++ src/app/donate/layout.tsx
@@ -10,8 +10,7 @@ const rubik = Rubik({
   variable: '--font-rubik',
 });
 
-import { NavComponent } from "@/components/component/navcomponent";
-import { Footer } from '@/components/footer';
+
 
 type LayoutProps = {
   children: React.ReactNode;  // Explicitly typing the children prop
@@ -19,24 +18,19 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   const initialOptions = {
-    clientId: "Aei8lGQxd0aIRyJTmhuGCTDj_8tCd_9cG9liz-9tKVkMfz8NgZGzQ4km-JmBQ_PuFcrKs1vvCHhUlAfG", // use camelCase for clientId
-  currency: "USD",
-  intent: "capture",
+    clientId: "Aei8lGQxd0aIRyJTmhuGCTDj_8tCd_9cG9liz-9tKVkMfz8NgZGzQ4km-JmBQ_PuFcrKs1vvCHhUlAfG", // Replace with actual client ID
+    currency: "USD",
+    intent: "capture",
     // Include any other options you need
   };
   
   return (
-    <html lang="en">
-      <head>
-        {/* Rubik font loaded with style */}
-        <style>{`:root { --font-rubik: ${rubik.style.fontFamily};}`}</style>
-      </head>
-      <body className={rubik.variable}>
-        <NavComponent />
-        <PayPalScriptProvider options={initialOptions}>
-          {children}
-        </PayPalScriptProvider>
-      </body>
-    </html>
+    <>
+     
+      <PayPalScriptProvider options={initialOptions}>
+        {children}
+      </PayPalScriptProvider>
+      
+    </>
   );
 }
