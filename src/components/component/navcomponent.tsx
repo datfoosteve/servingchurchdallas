1338a1e2c0fad15fc382cdd7cb05eb1b497ef7dd
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2024-04-30 11:58:35 -0500

    Update navigation component to use new About Us link

--- src/components/component/navcomponent.tsx
+++ src/components/component/navcomponent.tsx
@@ -130,7 +130,7 @@ export function NavComponent() {
           <NavigationMenuLink asChild>
             <Link
               className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50"
-              href="/contact-us/contact-church"
+              href="/about-us"
             >
               About Us
             </Link>
---
+++ src/images/icons/ArrowRightIcon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ArrowRightIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+    return (
+      <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M5 12h14" />
+        <path d="m12 5 7 7-7 7" />
+      </svg>
+    )
+  }
+
+  export default ArrowRightIcon;
