e78002c580c4a44b03a136636fb78a8594de18c7
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2024-05-22 05:09:38 -0500

    Refactor SermonsPage component and update styles

--- src/app/sermons/page.tsx
+++ src/app/sermons/page.tsx
@@ -2,18 +2,18 @@
 import React from 'react';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 
-
 const SermonsPage = () => {
   return (
-    <div className="flex container mx-auto px-2 py-8 shadow-border">
-      <Card>
+    <div className="container mx-auto px-2 py-8">
+      <Card className="shadow-lg">
         <CardContent>
-          <CardTitle className="text-4xl font-bold mb-4 content-center text-center">Sermons</CardTitle>
-          <div className="flex container w-screen">
+          <CardTitle className="text-4xl font-bold mb-4 text-center">Sermons</CardTitle>
+          <div className="flex flex-col items-center">
             <iframe
               src="https://embed.acast.com/63d6855a93dfbe00116a43c6?theme=light&font-family=Poppins&font-src=https%3A%2F%2Ffonts.googleapis.com%2Fcss%3Ffamily%3DPoppins&feed=true"
               title="sermons"
-              className="flex container grow w-3/4 h-screen box-content overflow-hidden shadow-border"
+              className="w-full h-screen max-w-4xl shadow-md"
+              style={{ border: 'none' }}
             ></iframe>
           </div>
         </CardContent>
